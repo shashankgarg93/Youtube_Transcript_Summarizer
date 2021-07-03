7a25c76962c86050ec9d666cdff6358062c41e83
Shashank Garg <shashank.garg9300@gmail.com>
2021-07-03 17:01:58 +0530

    abs_summarizer created

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from datetime import datetime
 from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # define a variable to hold you app
 # app = Flask(__name__)
@@ -25,4 +26,22 @@ def get_transcript(id):
         str_transcript+=detail['text']+" "
     return str_transcript
 
-# print(get_transcript("ezlG2DyiXM0"))
+def abs_summarizer(transcript):
+    # initialize the model architecture and weights
+    model = T5ForConditionalGeneration.from_pretrained("t5-base")
+    # initialize the model tokenizer
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    inputs = tokenizer.encode("summarize: " + transcript, return_tensors="pt", max_length=1024, truncation=True)
+    outputs = model.generate(
+    inputs, 
+    max_length=300, 
+    min_length=50, 
+    length_penalty=2.0, 
+    num_beams=4, 
+    early_stopping=True)
+# just for debugging
+    print(outputs)
+    print(tokenizer.decode(outputs[0]))
+
+
+(abs_summarizer(get_transcript("ezlG2DyiXM0")))
