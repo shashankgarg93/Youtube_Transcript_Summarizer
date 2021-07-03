4f8601da811f438b02eedc18521910c58cc72fac
Shashank Garg <shashank.garg9300@gmail.com>
2021-07-03 23:35:05 +0530

    changed event listener

--- chrome_extension/contentScript.js
+++ chrome_extension/contentScript.js
@@ -1,29 +1,44 @@
 
   
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    
-    if (message === 'get-summary') {
-        var url = '';
-        chrome.tabs.getCurrent(function(tab){
-            url = tab.url;
-            console.log(tab.url);
-    });
-    const xhr = new XMLHttpRequest();
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    //alert("above if")
+    if (request.method === 'getComments') {
+        chrome.tabs.query({
+            'active': true,
+            'lastFocusedWindow': true
+        }, function(tabs) {
+            let serverUrl = tabs.url;
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'http://127.0.0.1:5000/api/summarize?youtube_url='+serverurl);            
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onload = () => {
+                sendResponse({
+                    arr: 'something'
+                }); //it seems this is not working
+            };
+            xhr.send();
+        });
+//         var url = '';
+//         chrome.tabs.getCurrent(function(tab){
+//             url = tab[0].url;
+//             console.log(tab.url);
+//     });
+//     const xhr = new XMLHttpRequest();
 
 
-// create a `GET` request
-xhr.open('GET', 'http://127.0.0.1:5000/api/summarize?youtube_url='+url);
+// // create a `GET` request
+// xhr.open('GET', 'http://127.0.0.1:5000/api/summarize?youtube_url='+url, true);
 
-// send request
-xhr.send();
-var summary = '';
-xhr.onload = () => {
-    // get JSON response
-    const user = xhr.response;
-summary = user;
-    // log details
-    console.log(user); // John Doe
-}
-chrome.runtime.sendMessage(user);
-    }
-  });
+// // send request
+// xhr.send();
+// var summary = '';
+// xhr.onload = () => {
+//     // get JSON response
+//     const user = xhr.response;
+// summary = user;
+//     // log details
+//     sendResponse({message:"hi hello"});
+//  // John Doe
+// }
+return true;
+  }});
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,10 +1,13 @@
 document.getElementById("Summarize").addEventListener("click",
 function()
 {
-    chrome.runtime.sendMessage('get-summary');
-    chrome.runtime.onMessage.addListener(
-      (message,sender,sendResponse)=>{
-        document.getElementById("summary").innerHTML(<p>message</p>)
-      })
-})
+    chrome.runtime.sendMessage({method:'getComments'}
+    ,(response)=>{
+      document.getElementById("summary").innerHTML=response.arr;
+    });
+    // chrome.runtime.onMessage.addListener(
+    //   (message,sender,sendResponse)=>{
+    //     document.getElementById("summary").innerHTML=message;
+    //   });
+});
 
