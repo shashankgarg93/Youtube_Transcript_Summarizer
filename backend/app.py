6157da63214d131f1207a3f7cbe4a191b971dc24
Shashank Garg <shashank.garg9300@gmail.com>
2021-07-03 16:21:26 +0530

    get_transcript function created

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from datetime import datetime
+from youtube_transcript_api import YouTubeTranscriptApi
 
 # define a variable to hold you app
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# define your resource endpoints
-app.route('/')
-def index_page():
-    return "Hello world"
+# # define your resource endpoints
+# app.route('/')
+# def index_page():
+#     return "Hello world"
 
-app.route('/time', methods=['GET'])
-def get_time():
-    return str(datetime.datetime.now())
+# app.route('/time', methods=['GET'])
+# def get_time():
+#     return str(datetime.datetime.now())
 
-# server the app when this file is run
-if __name__ == '__main__':
-    app.run()
+# # server the app when this file is run
+# if __name__ == '__main__':
+#     app.run()
+
+def get_transcript(id):
+    str_transcript = ""
+    transcript = YouTubeTranscriptApi.get_transcript(id,languages=['en-IN','en'])
+    for detail in transcript:
+        str_transcript+=detail['text']+" "
+    return str_transcript
+
+# print(get_transcript("ezlG2DyiXM0"))
